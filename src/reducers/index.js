3aabe8c5168a351c86a82aed5beb3fc3603e9fbd
Your Name <toanntq@devcamp.edu.vn>
2023-02-06 21:51:20 +0700

    update index reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from "redux";
 
 import customerReducers from "./customer.reducers";
+import orderReducers from "./order.reducers";
+import orderDetailReducers from "./orderDetail.reducers";
 import productReducers from "./product.reducers";
 const rootReducer = combineReducers({
     customerReducers,
+    orderReducers,
+    orderDetailReducers,
     productReducers
 });
 
