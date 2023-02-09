8f0ad508b8ba8dd3001a714d645eccd94b1ee31f
Your Name <toanntq@devcamp.edu.vn>
2023-02-09 22:37:36 +0700

    update Customer router

--- src/components/orderPage/NewOrder.js
+++ src/components/orderPage/NewOrder.js
@@ -8,131 +8,45 @@ import { useEffect } from 'react';
 import { FormControl, InputLabel, Select, Grid, Paper, MenuItem, Link, TextField } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { createNewOrder, fetchCities, fetchCountries, fetchOrder, getAddress, getCity, getCountry } from '../../actions/order.actions';
-
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    bgcolor: 'background.paper',
-    border: '0.1 solid pink',
-    width:"60%",
-    boxShadow: 24,
-    p: 4,
-};
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { OrderStepper } from './NewOrder/OrderStepper';
 
 export const NewOrder = () => {
     const dispatch = useDispatch();
     const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
 
-    const {
-        loadCountriesPending,
-        countryOptions,
-        cityOptions,
-        country,
-        address,
-        city,
-        createOrder,
-    } = useSelector((reduxData) => reduxData.orderReducers);
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
 
     useEffect(() => {
         dispatch(fetchCountries());
     }, []);
-
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        dispatch(createNewOrder(data));
-    };
-
-    const handleCountryChange = (event) => {
-        dispatch(getCountry(event.target.value));
-        dispatch(fetchCities(event.target.value));
-    }
-
-    const handleCityChange = (event) => {
-        dispatch(getCity(event.target.value));
-    }
-
-    const handleAddressChange = (event) => {
-        dispatch(getAddress(event.target.value));
-    }
-
+  
     return (
         <React.Fragment>
-            <Button onClick={handleOpen} variant="contained" startIcon={<Iconify icon="eva:plus-fill" />}>New Order</Button>
-            <Modal
-                keepMounted
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="keep-mounted-modal-title"
-                aria-describedby="keep-mounted-modal-description"
-            >
-                <Box sx={style}>
-                    <Typography id="keep-mounted-modal-title" variant="h6" component="h2">
-                        New Order
-                    </Typography>
-                    <Box sx={{ mt: 2, display: 'flex', flexDirection: 'column', alignItems: 'center' }} >
-                        <Box component="form" noValidate onSubmit={handleSubmit}>
-                            <Grid container spacing={2}>
-                                <Grid item xs={12} sm={6}>
-                                    <TextField size="small" name="firstName" required fullWidth id="firstName" label="First Name"/>
-                                </Grid>
-                                <Grid item xs={12} sm={6}>
-                                    <TextField size="small" required fullWidth id="lastName" label="Last Name" name="lastName" />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField size="small" required fullWidth id="phone" label="Phone" name="phone" />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField size="small" required fullWidth id="email" label="Email" name="email" />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <FormControl size="small" required fullWidth>
-                                        <InputLabel id="select-country">Country</InputLabel>
-                                        <Select onChange={handleCountryChange} labelId="select-country" autoWidth id="country" label="Country" name="country" value={country}>
-                                            {countryOptions ?
-                                                countryOptions.map((countryOption, index) => <MenuItem key={countryOption.id} value={countryOption.iso2}>{countryOption.name}</MenuItem>) :
-                                                null
-                                            }
-                                        </Select>
-                                    </FormControl>
-                                </Grid>
-
-                                <Grid item xs={12}>
-                                    <FormControl size="small" required fullWidth>
-                                        <InputLabel id="select-city">City</InputLabel>
-                                        <Select onChange={handleCityChange} size="small" required autoWidth id="city" labelId="select-city" label="City" name="city" value={city}>
-                                            {cityOptions ?
-                                                cityOptions.map((cityOptions, index) => <MenuItem key={cityOptions.id} value={cityOptions.name}>{cityOptions.name}</MenuItem>) : null
-                                            }
-                                        </Select>
-                                    </FormControl>
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField onChange={handleAddressChange} size="small" required fullWidth id="address" label="Address" name="address" value={address} />
-                                </Grid>
-                            </Grid>
-                            <Grid container justifyContent="flex-end" spacing={2}>
-                                <Grid item>
-                                    <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
-                                        Send
-                                    </Button>
-                                </Grid>
-                                <Grid item>
-                                    <Button onClick={handleClose} variant="contained" color='warning' sx={{ mt: 3, mb: 2 }}>
-                                        Cancel
-                                    </Button>
-                                </Grid>
-                            </Grid>
-                        </Box>
-                    </Box>
-                </Box>
-            </Modal>
+            <Button onClick={handleClickOpen} variant="contained" startIcon={<Iconify icon="eva:plus-fill" />}>
+                New Order
+            </Button>
+            <Dialog open={open} onClose={handleClose}  >
+                <DialogTitle>New Order</DialogTitle>
+                <DialogContent>
+                    
+                    <OrderStepper/>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                </DialogActions>
+            </Dialog>
         </React.Fragment>
     );
 }
