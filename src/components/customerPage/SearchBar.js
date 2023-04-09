5a3b9d972b8c1af06751db060fa6339a81ab931e
Your Name <n.t.quoctoan@gmail.com>
2023-04-09 23:33:48 +0700

    update Search customer

--- src/components/customerPage/SearchBar.js
+++ src/components/customerPage/SearchBar.js
@@ -16,29 +16,27 @@ const SEARCH_KEY = [
 export const SearchBar = () => {
 
     const dispatch = useDispatch()
-    const [key, setKey] = useState("")
-    const [value, setValue] = useState("")
     const { customerLoadCondition } = useSelector(reduxData => reduxData.customerReducers)
-
-
-
-    const handleClickFind =async () => {
-        await dispatch(updateCustomerLoadCondition({ [key]: value }))
-        console.log(customerLoadCondition)
+    const [key, setKey] = useState(Object.keys(customerLoadCondition) || "")
+    const [value, setValue] = useState(Object.values(customerLoadCondition) || "")
+    const handleClickFind = () => {
+        console.log(value)
+        if (!value) {
+            dispatch(updateCustomerLoadCondition({ [key]: value }))
+        }
     }
-
 
     return (
         <Grid container columnSpacing={1} mb={2} spacing={2} >
             <Grid item xs={4} sm={8} md={2} >
-                <Select fullWidth defaultValue="" size="small" onChange={e => setKey(e.target.value)}>
+                <Select fullWidth value={key} size="small" onChange={e => setKey(e.target.value)}>
                     {SEARCH_KEY.map((value, index) => (
                         <MenuItem key={index} value={value.key} >{value.display}</MenuItem>
                     ))}
                 </Select>
             </Grid>
             <Grid item xs={8} sm={8} md={8} >
-                <TextField fullWidth size="small" label={"Find customer with " + key} name={key} value={value} onChange={(e) => setValue(e.target.value)} />
+                <TextField fullWidth type="text" size="small" label={"Find customer with " + key} name={key} value={value} onChange={(e) => setValue(e.target.value)} />
             </Grid>
             <Grid item xs={12} sm={4} md={2}>
                 <Button variant="contained" sx={{ p: 1 }} fullWidth onClick={handleClickFind}>Find</Button>
